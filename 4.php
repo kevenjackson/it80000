9e4ad3c1588800b22a47d8cf3671ab999d9181c1
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-01-14 15:23:25 +0800

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,14 @@
+<ul class="marqueeleft">
+<li><a href="/index.php?route=product/category&path=1"><img src="/image/catalog/logo/kaspersky/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=3"><img src="/image/catalog/logo/microsoft/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=7"><img src="/image/catalog/logo/apple/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=5"><img src="/image/catalog/logo/adobe/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=5"><img src="/image/catalog/logo/corel/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=2_207"><img src="/image/catalog/logo/avg/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=2"><img src="/image/catalog/logo/eset/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=2_208"><img src="/image/catalog/logo/mcafee/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=2_209"><img src="/image/catalog/logo/trendmicro/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=5&product_id=73"><img src="/image/catalog/logo/idm/1.gif"></a></li>
+<li><a href="/index.php?route=product/category&path=5&product_id=74"><img src="/image/catalog/logo/thunder/1.gif"></a></li></ul>
+
+<script>$(".div4").slide({mainCell: ".marqueeleft", effect: "leftMarquee", vis: 10, interTime: 1,});</script>
