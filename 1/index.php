8a922369ccd120a87c05f92d668412596a76f13e
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-01-14 15:28:04 +0800

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,187 @@
+﻿<!-- InstanceBegin template="/Templates/index.dwt.php" codeOutsideHTMLIsLocked="false" --><head><meta charset="UTF-8"><base target="_blank">
+<!-- InstanceBeginEditable name="head" --><meta name="author" content="奔浪网 陈天发 kevenjackson" />
+<meta name="keywords" content="奔浪网,上海创威贸易有限公司Shang Hai it-software Co.Ltd,alibaba,aliexpress.alipay,taobao,tmail,shop,amazon,ebay,paypal,payza,visa,credit card,master card,maestro,jcb,american express,国际信用卡付款收单,China union pay,中国银联,奔浪手机电脑,电脑加速,系统精减及优化,政府企业网站建设,电子商务外贸网站建设,平面网页设计,三维设计,3D VR三维全景图设计与制做,建筑装修设计,视频及电子相册制做,数码电子相册书打印复印,pdf编辑修改导出为word,excel,txt,奔浪3D VR视频网,电影,美女搞笑视频直播," /><!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="doctitle" --><title>奔浪网---专业互联网电脑商务及信息技术服务商</title>
+
+<link rel="stylesheet" type="text/css" href="/cnstyle.css" /><!-- InstanceEndEditable -->
+<style>.div1 {background: url(banner.jpg) center no-repeat; background-size: 100% 100%; filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='scale');}</style>
+<script src="/scripts/vue/vue.js"></script>
+<script src="/scripts/vue/vueslideshow.js"></script>
+<script src="/scripts/jquery/jquery.js"></script>
+<script src="/scripts/jquery/jquerycycle.js"></script>
+<script src="/scripts/jquery/jqueryslide.js"></script>
+<script src="/scripts/jquery/jqueryzoom.js"></script>
+<script src="/scripts/jquery/jqueryelevatezoom.js"></script>
+<script src="/scripts/jquery/jqueryqrcode.js"></script>
+<script src="/scripts/ajax/ajax.js"></script>
+<script src="/scripts/3d/ge1doot.js"></script>
+<script src="/scripts/3d/ga.js"></script>
+<script src="/scripts/3d/imageTransform3D.js"></script>
+<script src="/scripts/3d/3dpano.js"></script>
+<script src="/scripts/3d/3dconfig.js"></script>
+<script src="/scripts/serviceqq/1.js"></script>
+<script src="/scripts/serviceqq/2.js"></script>
+<script src="/scripts/share/share.js"></script></head><body>
+
+<div class="div1"><img class="left01" src="/image/catalog/logo/logo/1.png"></div>
+<div class="div2" id="1"><script>$("#1").load("1.php");</script></div>
+
+<div class="div3" id="slide"><!-- InstanceBeginEditable name="2" -->
+<script>new Vue().VueSlideShow("#slide", {images: 
+[{src: "/image/catalog/design/1.jpg", href: "#1"},
+{src: "/image/catalog/design/2.jpg", href: "#2"},
+{src: "/image/catalog/design/3.jpg", href: "#3"},
+{src: "/image/catalog/design/4.jpg", href: "#4"},
+{src: "/image/catalog/design/5.jpg", href: "#5"},
+{src: "/image/catalog/design/6.jpg", href: "#6"},
+{src: "/image/catalog/design/7.jpg", href: "#7"},
+{src: "/image/catalog/design/8.jpg", href: "#8"},
+{src: "/image/catalog/design/9.jpg", href: "#9"},
+{src: "/image/catalog/design/10.jpg", href: "#10"},],
+config: {effect: "slide", autoplay: 2000,}});</script><!-- InstanceEndEditable --></div>
+
+<div class="div3" id="fade"><!-- InstanceBeginEditable name="3" -->
+<script>new Vue().VueSlideShow("#fade", {images: 
+[{src: "/image/catalog/design/11.jpg", href: "#11"},
+{src: "/image/catalog/design/12.jpg", href: "#12"},
+{src: "/image/catalog/design/13.jpg", href: "#13"},
+{src: "/image/catalog/design/14.jpg", href: "#14"},
+{src: "/image/catalog/design/15.jpg", href: "#15"},
+{src: "/image/catalog/design/16.jpg", href: "#16"},
+{src: "/image/catalog/design/17.jpg", href: "#17"},
+{src: "/image/catalog/design/18.jpg", href: "#18"},
+{src: "/image/catalog/design/19.jpg", href: "#19"},
+{src: "/image/catalog/design/20.jpg", href: "#20"},],
+config: {effect: "fade", autoplay: 2000,}});</script><!-- InstanceEndEditable --></div>
+
+<div class="div4"><canvas id="canvas" class="height475"><!-- InstanceBeginEditable name="4" --><script src="/scripts/3d/3dimages/1.js"></script><!-- InstanceEndEditable --></canvas></div>
+
+<div class="div3" id="3d">
+<script>if (isHTML5possible()) {creatViewer("3d", "/scripts/3d/3dconfig.js", "100%", "100%");}
+else {document.getElementById("nonhtml5").style.display = "inline";}
+window.onload = function() {document.getElementById("copyRight").style.display = "none";
+var aa = document.getElementById("copyRight");aa.innerHTML = "";}</script>
+<!-- InstanceBeginEditable name="5" -->
+<script>/*全景图地址，预览图使用横向或纵向条形图，全景图支持两种模式：立方体6面图，立方体条形图；*/
+preview = "/image/catalog/design/1.jpg";
+/*立方体6面图效果*/
+panoType = "cube";
+/*如果要使用立方体条形图效果，则使用下面的代码*/
+// panoType = "cubestrip";
+// cubestrip = "/image/catalog/product/1/8.jpg";
+cube_f = "/image/catalog/design/2.jpg";
+cube_r = "/image/catalog/design/3.jpg";
+cube_b = "/image/catalog/design/4.jpg";
+cube_l = "/image/catalog/design/5.jpg";
+cube_u = "/image/catalog/design/6.jpg";
+cube_d = "/image/catalog/design/7.jpg";</script><!-- InstanceEndEditable --></div>
+
+<div class="div3" id="vr"><!-- InstanceBeginEditable name="6" -->
+<script>new Vue().VueSlideShow("#vr", {images: 
+[{src: "/image/catalog/design/1.jpg", href: "#1"},
+{src: "/image/catalog/design/2.jpg", href: "#2"},
+{src: "/image/catalog/design/3.jpg", href: "#3"},
+{src: "/image/catalog/design/4.jpg", href: "#4"},
+{src: "/image/catalog/design/5.jpg", href: "#5"},
+{src: "/image/catalog/design/6.jpg", href: "#6"},
+{src: "/image/catalog/design/7.jpg", href: "#7"},
+{src: "/image/catalog/design/8.jpg", href: "#8"},
+{src: "/image/catalog/design/9.jpg", href: "#9"},
+{src: "/image/catalog/design/10.jpg", href: "#10"},
+{src: "/image/catalog/design/11.jpg", href: "#11"},
+{src: "/image/catalog/design/12.jpg", href: "#12"},
+{src: "/image/catalog/design/13.jpg", href: "#13"},
+{src: "/image/catalog/design/14.jpg", href: "#14"},
+{src: "/image/catalog/design/15.jpg", href: "#15"},
+{src: "/image/catalog/design/16.jpg", href: "#16"},
+{src: "/image/catalog/design/17.jpg", href: "#17"},
+{src: "/image/catalog/design/18.jpg", href: "#18"},
+{src: "/image/catalog/design/19.jpg", href: "#19"},
+{src: "/image/catalog/design/20.jpg", href: "#20"},],
+config: {effect: "slide", autoplay: 2000,}});</script><!-- InstanceEndEditable --></div>
+
+<div class="div4"><!-- InstanceBeginEditable name="7" -->
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl>
+
+<dl><dd><div><img src="/image/catalog/design/1.jpg"></div>
+<div class="rotate"><img src="/image/catalog/design/2.jpg"></div></dd>
+<dt><a href="/www/design">专业平面网页动画三维装修设计</a></dt></dl><!-- InstanceEndEditable --></div>
+
+<div class="div4 img100" id="4"><script>$("#4").load("4.php");</script></div>
+<div class="div4 img100" id="5"><script>$("#5").load("5.php");</script></div>
+<div class="div12" id="6"><script>$("#6").load("6.php");</script>
+<?php require("/count.php"); ?><br></div></body><!-- InstanceEnd -->
