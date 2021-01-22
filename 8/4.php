76cf16ff520cd67c6905992d8a826496880de5ac
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-01-22 13:47:38 +0800

    Add files via upload

---
+++ 8/4.php
@@ -0,0 +1,23 @@
+<ul class="marqueeleft">
+<li><a href="/www/design"><img src="/image/catalog/design/1.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/2.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/3.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/4.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/5.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/6.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/7.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/8.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/9.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/10.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/11.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/12.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/13.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/14.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/15.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/16.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/17.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/18.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/19.jpg"></a></li>
+<li><a href="/www/design"><img src="/image/catalog/design/20.jpg"></a></li></ul>
+
+<script>$(".div4").slide({mainCell: ".marqueeleft", effect: "leftMarquee", vis: 10, interTime: 1,});</script>
