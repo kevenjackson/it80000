f0071251f1dfe103fb95eddd0a7efd7b473662a5
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-02-03 14:25:00 +0800

    Delete finance directory

--- finance/2/index.php
+++
@@ -1,7 +0,0 @@
-<!doctype html><html><head><meta charset="UTF-8"><meta name="author" content="甫背新闻网 陈天发" /><meta name="keywords" content="中国甫背大开发-新闻资讯网" />
-<title>甫背互助基金-投资人股东名录</title>
-
-<link rel="stylesheet" type="text/css" href="/cnstyle.css" /><base ></head><body>
-
-<script language="javascript" src="/scripts/password/3.js" /></script>
-</body></html>
