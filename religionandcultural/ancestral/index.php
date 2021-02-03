d909252b5063588a75013379df8c900685bc54b4
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-02-03 14:25:45 +0800

    Delete religionandcultural/ancestral directory

--- religionandcultural/ancestral/index.php
+++
@@ -1,7 +0,0 @@
-<!doctype html><html><head><meta charset="UTF-8"><meta name="author" content="陈天发" /><meta name="keywords" content="中国甫背大开发-新闻资讯网" />
-<title>甫背中华陈氏族谱</title>
-
-<link rel="stylesheet" type="text/css" href="/cnstyle.css" /><base ></head><body>
-
-<script language="javascript" src="/scripts/password/2.js" /></script>
-</body></html>
