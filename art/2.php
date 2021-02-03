1782bed2a7efbdacde3f4a384ad15951df6b060c
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-02-03 14:22:06 +0800

    Delete art directory

--- art/2.php
+++
@@ -1,9 +0,0 @@
-<p align="center">甫背湖边书院-龙文书院<br>————读圣贤书立凌云志卧龙终将平天下，<br>————修宏图业求治国策山夫亦可救黎民。<br>
-文/陈天发</p>
-
-<p align="left">甫背龙文书院，原名甫背湖边书院，</p>
-
-<iframe class="w100" height="880" framescrolling="no" src="http://www.jt720.cn/t/8d5e810ef269722f"></iframe>
-
-<object width="100%" height="880" data="https://v.qq.com/iframe/player.html?vid=g01595lat9h&tiny=0&auto=1"></object><br>
-<object width="100%" height="865" data="https://v.qq.com/iframe/player.html?vid=r032599o7b2&tiny=0&auto=0"></object>
