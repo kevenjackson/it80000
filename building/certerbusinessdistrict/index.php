335e7fb4b4441aefb85f510eede3b9af29ac124a
kevenjackson <75757173+kevenjackson@users.noreply.github.com>
2021-02-03 14:17:47 +0800

    Add files via upload

---
+++ building/certerbusinessdistrict/index.php
@@ -0,0 +1,115 @@
+﻿<!-- InstanceBegin template="/Templates/leftright.dwt.php" codeOutsideHTMLIsLocked="false" --><head><meta charset="UTF-8"><base target="_blank">
+<!-- InstanceBeginEditable name="head" --><meta name="author" content="陈天发" /><meta name="keywords" content="奔浪网" /><!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="doctitle" --><title>original genuine Kaspersky anti-virus Kav 1y-1pc or device</title>
+
+<link rel="stylesheet" type="text/css" href="/cnstyle.css" />
+<style>.div1{background: url(/pb/banner.jpg) center no-repeat; background-size: 100% 100%; filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod='scale');}</style>
+<!-- InstanceEndEditable -->
+<script src="/scripts/vue/vue.js"></script>
+<script src="/scripts/vue/vueswiper.js"></script>
+<script src="/scripts/jquery/jquery.js"></script>
+<script src="/scripts/jquery/jqueryslide.js"></script>
+<script src="/scripts/jquery/jqueryzoom.js"></script>
+<script src="/scripts/jquery/jqueryelevatezoom.js"></script>
+<script src="/scripts/jquery/jqueryqrcode.js"></script>
+<script src="/scripts/ajax/ajax.js"></script>
+<script src="/scripts/3d/ge1doot.js"></script>
+<script src="/scripts/3d/3dimages.js"></script>
+<script src="/scripts/3d/ga.js"></script>
+<script src="/scripts/3d/imageTransform3D.js"></script>
+<script src="/scripts/3d/3dpano.js"></script>
+<script src="/scripts/3d/3dconfig.js"></script>
+<script src="/scripts/serviceqq/1.js"></script>
+<script src="/scripts/serviceqq/2.js"></script>
+<script src="/scripts/share/share.js"></script></head><body>
+
+<div class="div1"><!-- InstanceBeginEditable name="logo" --><img src="/image/catalog/logo/logo/1.png"><!-- InstanceEndEditable --></div>
+<div class="div2" id="1"><!-- InstanceBeginEditable name="1" --><script>$("#1").load("/pb/1.php");</script><!-- InstanceEndEditable --></div>
+
+<div class="div9"><dl class="w25">
+<dt><!-- InstanceBeginEditable name="left" --><span id="menu5"><script>$("#menu5").load("/shop/templates/1/menu5.php");</script></span><!-- InstanceEndEditable --></dt>
+<!-- InstanceBeginEditable name="11" --><dd id="11"><script>$("#11").load("1.php");</script></dd><!-- InstanceEndEditable -->
+
+<dt>product photo show产品展示</dt>
+<dd class="scroll" id="2"><div class="swiper-wrapper"><!-- InstanceBeginEditable name="2" -->
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kav/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kav/2.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kav/3.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kis/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kis/2.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kis/3.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kis/4.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kis/5.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kts/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kts/2.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kts/3.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/ksos/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/ksos/2.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/ksos/3.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kes/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/kms/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/kaspersky/ksc/1.gif"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/eav/1.jpg"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/ess/1.jpg"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/essp/1.jpg"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/eis/1.jpg"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/ems/1.jpg"></div>
+<div class="swiper-slide"><img src="/image/catalog/product/pcsoftware/eset/efs/1.jpg"></div><!-- InstanceEndEditable --></div>
+<div class="swiper-button-next"></div><div class="swiper-button-prev"></div>
+<script>var swiper = new Swiper('.scroll', {effect: 'slide', autoplay: {delay: 2000,}, slidesPerView: 1, images: 
+[{src: "/image/catalog/product/pcsoftware/kaspersky/kav/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kav/2.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kav/3.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kis/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kis/2.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kis/3.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kis/4.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kis/5.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kts/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kts/2.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kts/3.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/ksos/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/ksos/2.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/ksos/3.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kes/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/kms/1.gif"},
+{src: "/image/catalog/product/pcsoftware/kaspersky/ksc/1.gif"},
+{src: "/image/catalog/product/pcsoftware/eset/eav/1.jpg"},
+{src: "/image/catalog/product/pcsoftware/eset/ess/1.jpg"},
+{src: "/image/catalog/product/pcsoftware/eset/essp/1.jpg"},
+{src: "/image/catalog/product/pcsoftware/eset/eis/1.jpg"},
+{src: "/image/catalog/product/pcsoftware/eset/ems/1.jpg"},
+{src: "/image/catalog/product/pcsoftware/eset/efs/1.jpg"},],
+navigation: {nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev',},});</script></dd>
+
+<dt>3D-VR show三维全景图展示</dt>
+<dd><canvas id="canvas"></canvas></dd>
+
+<dd><p class="height400" id="3d"></p>
+<script>if (isHTML5possible()) {creatViewer("3d", "/scripts/3d/3dconfig.js", "100%", "100%");}
+else {document.getElementById("nonhtml5").style.display = "inline";}
+window.onload = function() {document.getElementById("copyRight").style.display = "none";
+var aa = document.getElementById("copyRight");aa.innerHTML = "";}</script>
+<!-- InstanceBeginEditable name="3" --><script>/*全景图地址，预览图使用横向或纵向条形图，全景图支持两种模式：立方体6面图，立方体条形图；*/
+preview = "/image/catalog/design/1.jpg";
+/*立方体6面图效果*/
+panoType = "cube";
+/*如果要使用立方体条形图效果，则使用下面的代码*/
+// panoType = "cubestrip";
+// cubestrip = "/image/catalog/product/1/8.jpg";
+cube_f = "/image/catalog/design/2.jpg";
+cube_r = "/image/catalog/design/3.jpg";
+cube_b = "/image/catalog/design/4.jpg";
+cube_l = "/image/catalog/design/5.jpg";
+cube_u = "/image/catalog/design/6.jpg";
+cube_d = "/image/catalog/design/7.jpg";</script><!-- InstanceEndEditable --></dd></dl>
+
+<dl class="w75 left5">
+<dt><!-- InstanceBeginEditable name="right" -->人杰地灵话甫背——甫背简介<!-- InstanceEndEditable --></dt>
+<!-- InstanceBeginEditable name="12" --><dd id="12"><script>$("#12").load("2.php");</script></dd><!-- InstanceEndEditable -->
+<span id="13"><script>$("#13").load("3.php");</script></span></dl></div>
+
+<div class="div4 img100" id="4"><!-- InstanceBeginEditable name="4" --><script>$("#4").load("/pb/4.php");</script><!-- InstanceEndEditable --></div>
+<div class="div4 img100" id="5"><!-- InstanceBeginEditable name="5" --><script>$("#5").load("/pb/5.php");</script><!-- InstanceEndEditable --></div>
+<div class="div12" id="6"><!-- InstanceBeginEditable name="6" --><script>$("#6").load("/pb/6.php");</script><!-- InstanceEndEditable -->
+<?php require("/count.php"); ?><br></div></body><!-- InstanceEnd -->
